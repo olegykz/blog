8e951b24533ba8fbe221caecdfba6499b8844250
Oleg Yakovenko <olegykz@gmail.com>
2016-11-13 15:10:36 +0200

    VK id changed

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,7 +11,7 @@ var login_params;
 var current_session;
 
 $(document).ready(function() {
-    VK.init({apiId: 2024149, onlyWidgets: false});
+    VK.init({apiId: 2848766});
 
     VK.Widgets.Auth("vk_auth", {
         width: "500px",
